921e481891ee12318e81f6292c4c0b0d096b43ec
samndambuki <sam.ndambuki08@gmail.com>
2025-06-30 15:44:04 +0300

    worked on logout and homepage design

--- src/app/components/top-bar/top-bar.ts
+++ src/app/components/top-bar/top-bar.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
 import { User } from '../../services/user';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-top-bar',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule,MatMenuModule,MatIconModule,CommonModule],
   templateUrl: './top-bar.html',
   styleUrl: './top-bar.scss'
 })
 export class TopBar implements OnInit {
-  constructor(private userService:User,private router:Router){}
+  constructor(public userService:User,private router:Router){}
   ngOnInit(): void {
     if(this.userService.user == undefined){
       let str = localStorage.getItem('user');
@@ -21,5 +24,10 @@ export class TopBar implements OnInit {
       this.router.navigate(['/login']);
       }
     }
+  }
+  logout(){
+    this.userService.user=undefined;
+    localStorage.clear();
+    this.router.navigate(['/login']);
   }
 }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatIconModule,MatButtonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
